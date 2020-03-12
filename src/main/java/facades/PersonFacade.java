fe8547d860096e153e06ee77b363f25a86ae92ce
cprejler <prejler93@gmail.com>
2020-03-12 15:26:58 +0100

    first commit

---
+++ src/main/java/facades/PersonFacade.java
@@ -0,0 +1,51 @@
+package facades;
+
+import entities.Person;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * Rename Class to a relevant name Add add relevant facade methods
+ */
+public class PersonFacade {
+
+    private static PersonFacade instance;
+    private static EntityManagerFactory emf;
+    
+    //Private Constructor to ensure Singleton
+    private PersonFacade() {}
+    
+    
+    /**
+     * 
+     * @param _emf
+     * @return an instance of this facade class.
+     */
+    public static PersonFacade getFacadeExample(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new PersonFacade();
+        }
+        return instance;
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+    
+    //TODO Remove/Change this before use
+    public long getPersonCount(){
+        EntityManager em = emf.createEntityManager();
+        try{
+            long renameMeCount = (long)em.createQuery("SELECT COUNT(p) FROM Person p").getSingleResult();
+            return renameMeCount;
+        }finally{  
+            em.close();
+        }
+        
+    }
+
+}
