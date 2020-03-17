8e63fff621427cef582520054324e6353349f59a
Malthorn1 <mikkelskott@gmail.com>
2020-03-18 00:33:22 +0100

    first

---
+++ src/main/java/facades/PhoneFacade.java
@@ -0,0 +1,162 @@
+package facades;
+
+import dto.PersonDTO;
+import entities.Address;
+import entities.Hobby;
+import entities.Person;
+import entities.Phone;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * Rename Class to a relevant name Add add relevant facade methods
+ */
+public class PhoneFacade {
+
+    private static PhoneFacade instance;
+    private static EntityManagerFactory emf;
+    
+    //Private Constructor to ensure Singleton
+    
+    
+    
+    /**
+     * 
+     * @param _emf
+     * @return an instance of this facade class.
+     */
+    public static PhoneFacade getFacadeExample(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new PhoneFacade();
+        }
+        return instance;
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+    
+        public long getPhoneCount(){
+        EntityManager em = emf.createEntityManager();
+        try{
+            long phoneCount = (long)em.createQuery("SELECT COUNT(p) FROM Phone p").getSingleResult();
+            return phoneCount;
+        }finally{  
+            em.close();
+        }
+        
+        }
+    
+        public PersonDTO getPersonByPhoneNumber(int number){
+        EntityManager em = emf.createEntityManager();
+        try{
+
+            List<Phone> query =  em.createQuery("SELECT c FROM Phone c where c.number like :number", Phone.class ) 
+                    .setParameter("number", number).getResultList() ;
+            
+            
+           // Person p  = em.createQuery("Select c from Person c where c.")
+            
+            Person p = em.find(Person.class, (long) query.get(0).getId()) ;
+            System.out.println(p.getFirstName());
+            
+            
+            
+            return new PersonDTO(p);
+        }finally{
+            em.close();
+        }
+    }
+    
+    
+    
+//    //TODO Remove/Change this before use
+//    public long getPersonCount(){
+//        EntityManager em = emf.createEntityManager();
+//        try{
+//            long personCount = (long)em.createQuery("SELECT COUNT(p) FROM Person p").getSingleResult();
+//            return personCount;
+//        }finally{  
+//            em.close();
+//        }
+//        
+//    }
+//    
+//    public List<PersonDTO> getAllPersons(){
+//        EntityManager em = emf.createEntityManager();
+//        try{
+//            TypedQuery<Person> q = em.createQuery("SELECT p from Person p", Person.class);
+//            List<Person> persons = q.getResultList();
+//            List<PersonDTO> pDTO = new ArrayList<>();
+//            for (Person person : persons) {
+//                PersonDTO pdto = new PersonDTO(person);
+//                pDTO.add(pdto);
+//            }
+//            return pDTO;
+//        }finally{
+//            em.close();
+//        }
+//    }
+//    
+//    
+//    public void addPerson(Person person){
+//        EntityManager em = emf.createEntityManager();
+//        try{
+//            em.getTransaction().begin();
+//            em.persist(person);
+//            em.getTransaction().commit();
+//        }finally{
+//            em.close();
+//        }
+//    }
+//    
+//    public Person addAddress(Person person, Address address){
+//        EntityManager em = emf.createEntityManager();
+//        person.setAddress(address);
+//        
+//        try{
+//            em.getTransaction().begin();
+//            em.merge(person);
+//            em.getTransaction().commit();
+//        }finally{
+//            em.close();
+//        }
+//        return person;
+//    }
+//    
+//    public Person addPhone(Person person, Phone phone){
+//        EntityManager em = emf.createEntityManager();
+//        person.addPhones(phone);
+//        
+//        try{
+//            em.getTransaction().begin();
+//            em.merge(person);
+//            em.getTransaction().commit();
+//        }finally{
+//            em.close();
+//        }
+//        return person;
+//    }
+//    
+//    public Person addHobby(Person person, Hobby hobby){
+//        EntityManager em = emf.createEntityManager();
+//        person.addHobby(hobby);
+//        
+//        try{
+//            em.getTransaction().begin();
+//            em.merge(person);
+//            em.getTransaction().commit();
+//        }finally{
+//            em.close();
+//        }
+//        return person;
+//    }
+
+}
---
+++ src/main/java/rest/PhoneResource.java
@@ -0,0 +1,69 @@
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dto.PersonDTO;
+import entities.Person;
+import utils.EMF_Creator;
+import facades.PersonFacade;
+import facades.PhoneFacade;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+//Todo Remove or change relevant parts before ACTUAL use
+@Path("phone")
+public class PhoneResource {
+
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(
+                "pu",
+                "jdbc:mysql://localhost:3307/ca2",
+                "dev",
+                "ax2",
+                EMF_Creator.Strategy.CREATE);
+    
+    //An alternative way to get the EntityManagerFactory, whithout having to type the details all over the code
+    //EMF = EMF_Creator.createEntityManagerFactory(DbSelector.DEV, Strategy.CREATE);
+    
+    private static final PhoneFacade FACADE = PhoneFacade.getFacadeExample(EMF);
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+            
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String demo() {
+        return "{\"msg\":\"Hello World\"}";
+    }
+
+    
+    @GET
+    @Path("test/{number}")
+    @Produces({MediaType.APPLICATION_JSON}) 
+    public String getPersonByPhoneNumber(@PathParam("number") int number) {
+        
+        PersonDTO p = FACADE.getPersonByPhoneNumber(number) ; 
+        System.out.println(p.getEmail());
+        String d = GSON.toJson(p); 
+        System.out.println(d);
+        
+      //  String d = new Gson().toJson(FACADE.getPersonByPhoneNumber(number));
+        
+        return d ; 
+    }
+    
+    
+    @Path("count")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getRenameMeCount() {
+        long count = FACADE.getPhoneCount();
+        //System.out.println("--------------->"+count);
+        return "{\"count\":"+count+"}";  //Done manually so no need for a DTO
+    }
+
+}
