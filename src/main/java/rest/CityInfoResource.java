5d765d68998a72a9403c6ba28a086c31559f5fb5
ATThorsen <thorsen.aske@gmail.com>
2020-03-22 10:14:19 +0100

    Cityinfo add

--- src/main/java/rest/CityInfoResource.java
+++ src/main/java/rest/CityInfoResource.java
@@ -2,10 +2,13 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import dto.CityInfoDTO;
 import facades.CityInfoFacade;
 import utils.EMF_Creator;
 import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -50,6 +53,15 @@ public class CityInfoResource {
         return GSON.toJson(FACADE.getPersonsByCityInfo(zipcode)); 
     }
     
+    @POST
+    @Path("add")
+    @Produces({MediaType.APPLICATION_JSON}) 
+    @Consumes({MediaType.APPLICATION_JSON}) 
+    public String addPhone(String cityInfo){
+        CityInfoDTO cDTO = GSON.fromJson(cityInfo, CityInfoDTO.class);
+        CityInfoDTO cityDTO = FACADE.addCity(cDTO.getZipCode(), cDTO.getCity(), cDTO.getcDTOID());
+        return GSON.toJson(cityDTO);
+    }
     
  
  
